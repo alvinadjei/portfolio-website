6a9b2c321468d9fac9d30f714b31e1f3fd39713c
alvinadjei <alvinadjei@college.harvard.edu>
2025-01-27 00:04:24 -0800

    Home page and dashboard scaffolding

---
+++ portfolio-website/app/page.tsx
@@ -0,0 +1,81 @@
+import Image from "next/image";
+import Link from "next/link";
+import '@/app/ui/global.css';
+
+export default function Page() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        
+        <div className="text-white text-for text-center sm:text-left font-[family-name:var(--font-roboto-mono)]">
+          <span className="text-tiel text-xs">Hi there! My name is</span>
+          <div className="text-6xl font-[family-name:var(--font-roboto-sans)]">
+            Alvin Adjei.
+          </div>
+          <p className="text-2xl text-lightgrey">
+            I'm a full stack<br />
+            software engineer.
+          </p>
+        </div>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <Link
+            className="rounded-3xl hover:rounded-xl border border-solid border-black/[.08] dark:border-white/[.145] transition-all duration-300 ease-linear flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="/about"
+            target=""
+            rel="noopener noreferrer"
+          >
+            Let me show you around!
+          </Link>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <Link
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </Link>
+        <Link
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </Link>
+        <Link
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </Link>
+      </footer>
+    </div>
+  );
+}
---
+++ portfolio-website/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#000e1a',
+        tiel: '#00c4a4',
+        lightgrey: '#c5c6c7',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
