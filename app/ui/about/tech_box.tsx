f01bcdfb3b185f54a610cf32bb2929bac6aad457
alvinadjei <alvinadjei@college.harvard.edu>
2025-02-02 12:28:14 -0800

    Tech box hover turned off on sm screens

--- app/ui/about/tech_box.tsx
+++ app/ui/about/tech_box.tsx
@@ -6,7 +6,7 @@ export default function TechBox({ logo }: {logo:string}) {
     const alt = logo.replace(/-/g, '.').replace('Sharp', "#").replace('PP', '++');
 
     return (
-        <div className="flex flex-col justify-center h-[100px] w-[120px] p-[20px] m-[8px] transition-all duration-150 bg-lightbg hover:bg-blue-950 border border-lightgrey rounded-lg">
+        <div className="flex flex-col justify-center h-[100px] w-[120px] p-[20px] m-[8px] transition-all duration-150 bg-lightbg md:hover:bg-blue-950 border border-lightgrey rounded-lg">
             <div className="flex justify-center">
                 <Image 
                     src={src}
