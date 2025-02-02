d66196b8645513e2a705298a5acf4a558ed6980e
alvinadjei <alvinadjei@college.harvard.edu>
2025-02-02 12:35:38 -0800

    Added Harvard stuff to bio

--- app/(dashboard)/about/page.tsx
+++ app/(dashboard)/about/page.tsx
@@ -24,9 +24,11 @@ export default function Page() {
                     </div> {/* profile pic */}
                     <div className="pt-[40px] text-[16px] font-[family-name:var(--font-roboto-sans)]"> {/* bio */}
                         <p>
-                            Hello! I&apos;m Alvin, and (depending on who you ask) I am a software engineer, biomedical engineer, neuroscientist, 
-                            and/or musician. I enjoy wearing a lot of different hats, tackling interesting problems, and learning by doing and building.
-                            Some of the technologies, languages, and frameworks I have used to do so are on this page.
+                            Hello! I&apos;m Alvin, and (depending on who you ask) I am a software engineer, biomedical engineer, 
+                            neuroscientist, and/or musician. I recently graduated from Harvard College with a B.A. in Biomedical 
+                            Engineering and a minor in Computer Science. I enjoy wearing a lot of different hats, tackling interesting
+                            problems, and learning by doing and building. Some of the technologies, languages, and frameworks I have 
+                            used to do so are on this page.
                         </p>
                         <br/>
                         <p>
