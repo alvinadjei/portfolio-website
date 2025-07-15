5d76b3922808ff46baa8287e3ca0514bbed55929
alvinadjei <alvinadjei@college.harvard.edu>
2025-07-15 16:26:15 -0400

    Minor fixes
    Messing around with styling, added basic tags to post

--- app/(dashboard)/blog/[slug]/page.tsx
+++ app/(dashboard)/blog/[slug]/page.tsx
@@ -12,18 +12,25 @@ export async function generateStaticParams() {
 }
 
 export default async function BlogPostPage({ params }: BlogPageProps) {
-  const { slug } = params;
+  const { slug } = await params;
 
   const allSlugs = getArticleSlugs();
   if (!allSlugs.includes(slug)) notFound();
 
-  const { frontmatter, content } = await getMdxSource(slug);
+  const { frontmatter, content } = getMdxSource(slug);
 
   return (
     <main className="p-8 max-w-2xl mx-auto font-[family-name:var(--font-roboto-mono)]">
-      <h1 className="text-3xl font-bold mb-3">{frontmatter.title}</h1>
-      <span className="text-sm text-gray-400">{frontmatter.date}</span>
-      <div></div>
+      <h1 className="text-4xl font-bold mb-4">{frontmatter.title}</h1>
+      <span className="text-md text-gray-400">{frontmatter.date}</span>
+      <ul className="flex mt-1 mb-3 text-sm gap-4 text-gray-400">
+        {frontmatter.tags.map((tag, index) => (
+                            <li key={index}>
+                                #{tag}
+                            </li>
+                        ))}
+      </ul>
+      <hr className="page-title-line"></hr>
       <article className="article">
         <MDXRemote source={content} />
       </article>
